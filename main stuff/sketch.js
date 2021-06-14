8ec3ac7232abf4d053a6cdf3b025e7f1184d8fc3
Anton Söffing <26146401+AntonSoeffing@users.noreply.github.com>
2021-06-14 10:32:56 +0200

    Fixed inconsistent naming and reorganized sprites

--- main stuff/sketch.js
+++ main stuff/sketch.js
@@ -17,7 +17,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   //images
-  helmSprite = loadImage('helm2.png');
+  helmetSprite = loadImage('sprites/helmet.png');
 
   // create an engine
   engine = Engine.create();
@@ -26,14 +26,14 @@ function setup() {
   engine.world.gravity.y = 0.5;
 
   // create two boxes and a ground
-  boxA = Bodies.rectangle(200, 200, 80, 80);
+  helmet = Bodies.rectangle(200, 200, 80, 80);
   comet = Bodies.circle(800, 40, 20);
   ground = Bodies.rectangle(400, 800, 810, 10, {
     isStatic: true, angle: Math.PI * 0.06
   });
 
   // add all of the bodies to the world
-  World.add(engine.world, [boxA, comet, ground]);
+  World.add(engine.world, [helmet, comet, ground]);
 
   // run the engine
   Engine.run(engine);
@@ -42,7 +42,7 @@ function setup() {
 function draw() {
   background(0);
 
-  drawSprite(boxA, helmSprite);
+  drawSprite(helmet, helmetSprite);
 
   fill(180);
   drawBody(comet)
@@ -54,8 +54,8 @@ function draw() {
 function keyPressed() {
   // is SPACE pressed?
   if (keyCode === 32) {
-    Body.applyForce(boxA,
-      {x: boxA.position.x, y: boxA.position.y},
+    Body.applyForce(helmet,
+      {x: helmet.position.x, y: helmet.position.y},
       {x: 0.015, y: -0.15}
     );
     Body.applyForce(comet,
